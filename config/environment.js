9adbf1b507dab6e44ae40edf056b2fff88adbda8
NjeriNjoroge <wanjorogegrace@gmail.com>
2017-07-16 08:54:54 +0300

    added a firebase and linked it successfully

--- config/environment.js
+++ config/environment.js
@@ -18,6 +18,13 @@ module.exports = function(environment) {
       }
     },
 
+    firebase: {
+      apiKey: "AIzaSyDCfoed-k_ThtDPHUUef0N1nU84a3T0EhY",
+    authDomain: "questionboard-31843.firebaseapp.com",
+    databaseURL: "https://questionboard-31843.firebaseio.com",
+    storageBucket: "",
+  },
+
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
