5b73df3a9a92d7826c37484534afc30b254884fd
NjeriNjoroge <wanjorogegrace@gmail.com>
2017-07-16 13:47:59 +0300

    users can now contribute an answer to a particular question and view their answer to

--- app/models/question.js
+++ app/models/question.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   swali: DS.attr(),
   answer: DS.attr(),
   author: DS.attr(),
-  notes: DS.attr()
+  notes: DS.attr(),
+  answers: DS.hasMany('answer', {async: true})
 });
